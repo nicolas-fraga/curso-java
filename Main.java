ae52db5c02c3627251f79bcde37168c3e9da400d
nicolas-fraga <nicolasnssn@gmail.com>
2021-01-08 07:49:42 -0300

    exercicio 1

--- Main.java
+++ Main.java
@@ -1,8 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
+		double largura = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+		double metroQuadrado = sc.nextDouble();
+		
+		double area = comprimento * largura;
+		double preco = metroQuadrado * area;
+		
+		System.out.printf("AREA = %.2f%nPRECO = %.2f%n", area, preco);
+		sc.close();
 	}
 }
